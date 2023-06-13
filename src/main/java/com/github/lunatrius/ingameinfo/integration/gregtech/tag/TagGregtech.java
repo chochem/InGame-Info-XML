6efffb21314083312ad72be61e15a64ad7454e23
chochem <40274384+chochem@users.noreply.github.com>
2023-06-13 12:41:44 +0100

    add gt module

---
+++ src/main/java/com/github/lunatrius/ingameinfo/integration/gregtech/tag/TagGregtech.java
@@ -0,0 +1,30 @@
+package com.github.lunatrius.ingameinfo.integration.gregtech.tag;
+
+import com.github.lunatrius.ingameinfo.tag.TagIntegration;
+import com.github.lunatrius.ingameinfo.tag.registry.TagRegistry;
+import gregtech.common.GT_Worldgenerator;
+
+public abstract class TagBloodMagic extends TagIntegration {
+
+    @Override
+    public String getCategory() {
+        return "gregtech";
+    }
+
+    public static class useNewOregenPattern extends TagGregtech {
+
+        @Override
+        public String getValue() {
+            try {
+                return String.valueOf(GT_Worldgenerator.useNewOregenPattern);
+            } catch (Throwable e) {
+                log(this, e);
+            }
+            return "-1";
+        }
+    }
+
+    public static void register() {
+        TagRegistry.INSTANCE.register(new useNewOregenPattern().setName("gtnewore").setAliases("gtneworegenpattern"));
+    }
+}
